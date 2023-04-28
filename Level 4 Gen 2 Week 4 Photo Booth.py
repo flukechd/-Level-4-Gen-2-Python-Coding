313a5ad1c99df25901d3c082c0094598648b3b2a
flukechd <73994876+flukechd@users.noreply.github.com>
2023-04-28 13:54:36 +0700

    Add files via upload

---
+++ Level 4 Gen 2 Week 4 Photo Booth.py
@@ -0,0 +1,63 @@
+# PHOTOBOOTH
+import cv2
+import os
+import numpy as np
+
+print("\u2764\ufe0f")
+# Path to the folder containing the images
+folder_path = os.path.join('OutputImages')
+
+# List all image files in the folder
+image_files = [f for f in os.listdir(folder_path) if f.endswith('.jpg')]
+
+# Create a new blank image to hold the 4 images in a 4x4 layout
+result = None
+
+# Iterate over the first 4 image files
+for i in range(4):
+    # Open the image file
+    image = cv2.imread(os.path.join(folder_path, image_files[i]))
+
+    # Crop image
+    if i == 0:
+        # Create a blank mask image of the same size as the input image
+        blank = np.zeros(image.shape[:2], dtype='uint8')
+        # cv2.imshow('mask',mask)
+        mask = cv2.circle(blank.copy(), (310, 210), 220, 255, -1)
+        # cv2.imshow('c',circle)
+        image = cv2.bitwise_and(image, image, mask=mask)
+
+        # Convert Color spaces
+    elif i == 1:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+
+    # Smoothing
+    elif i == 2:
+        image = cv2.blur(image, (7, 7))
+
+    # RGB Splitmerge
+    elif i == 3:
+        blank = np.zeros(image.shape[:2], dtype='uint8')
+        b, g, r = cv2.split(image)
+        image = cv2.merge([b, blank, blank])
+
+    # Resize the image to fit a 200x200 square
+    image = cv2.resize(image, (400, 400))
+
+    # Add a white border around the image
+    image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(255, 255, 255))
+
+    # Add the image to the result image
+    if result is None:
+        result = image
+    else:
+        result = cv2.hconcat([result, image])
+
+# Add black borders between the images
+result = cv2.copyMakeBorder(result, 40, 40, 40, 40, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+
+# Show the result image
+cv2.imshow("Result", result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
